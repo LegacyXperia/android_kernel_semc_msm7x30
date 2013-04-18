3b58b195c474d55bc9bf31dda6a3602728f1c574
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-04-18 19:50:21 +0200

    cpufreq: BrazilianWax CPU governor
    A slightly more agressive smart optimized governor
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_brazilianwax.c
    	modified:   include/linux/cpufreq.h
    
    Change-Id: I16d147b1de93024315e81b26ead9b88e1a650070

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -360,6 +360,9 @@ extern struct cpufreq_governor cpufreq_gov_performance;
 #endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
+extern struct cpufreq_governor cpufreq_gov_brazilianwax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_brazilianwax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
