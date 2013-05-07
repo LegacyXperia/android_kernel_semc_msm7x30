01f4978b25479b8d87e08b1173048137c7347768
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-05-07 23:00:51 +0200

    cpufreq: InteractiveX CPU governor
    Modified version of interactive with sleep+wake code
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_interactivex.c
    	modified:   include/linux/cpufreq.h
    
    Change-Id: I385aed39696997df385bd3f91102d62b9b4ac849

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactivex)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
