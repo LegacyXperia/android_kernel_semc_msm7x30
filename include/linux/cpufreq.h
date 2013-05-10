e4ba11d05f7f2e750cb075628eb537884d8e34fe
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-05-10 20:49:18 +0200

    cpufreq: Hyper CPU governor
    A tweaked OnDemand based smart and smooth optimized governor
    
    Conflicts:
    	drivers/cpufreq/Makefile
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_hyper.c
    	modified:   include/linux/cpufreq.h
    
    Change-Id: Ieb07eac957950c45da3b1794fe354ed7437c7962

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -363,6 +363,9 @@ extern struct cpufreq_governor cpufreq_gov_performance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_hyper;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hyper)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
