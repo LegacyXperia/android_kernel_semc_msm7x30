fa73b1b3200745ec7400347a94030410ac195e48
Dennis Rassmann <showp1984@gmail.com>
2013-07-31 02:04:25 +0200

    -o3 fix

--- fs/namei.c
+++ fs/namei.c
@@ -2931,7 +2931,7 @@ SYSCALL_DEFINE3(symlinkat, const char __user *, oldname,
 		int, newdfd, const char __user *, newname)
 {
 	int error;
-	char *from;
+	char *from = NULL;
 	struct dentry *dentry;
 	struct path path;
 
