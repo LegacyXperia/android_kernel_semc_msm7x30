0154eb9ecd64ff750fb4cee32f803e2f09eea2ec
Michael Bestas <mikeioannina@gmail.com>
2013-09-11 22:59:10 +0300

    mogami: Configure charger
    Change-Id: Icb059187bd9e415ef126f7bc420e33738a158823

---
+++ arch/arm/mach-msm/charger-satsuma.c
@@ -0,0 +1,25 @@
+/* arch/arm/mach-msm/charger-satsuma.c
+ *
+ * Copyright (C) 2010 Sony Ericsson Mobile Communications AB.
+ * Copyright (C) 2013 LegacyXperia Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+#include <linux/battery_chargalg.h>
+
+static struct battery_regulation_vs_temperature id_bat_reg = {
+	/* Cold, Normal, Warm, Overheat */
+	{5, 45,		55,	127},	/* battery temp */
+	{0, 4200,	4000,	0},	/* charge volt */
+	{0, USHRT_MAX,	400,	0},	/* charge curr */
+};
+
+struct device_data device_data = {
+	.id_bat_reg = &id_bat_reg,
+	.battery_capacity_mah = 1200,
+	.maximum_charging_current_ma = 850,
+};
