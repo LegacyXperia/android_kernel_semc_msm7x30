5b67fdd360fb5fc44f70b208baa9d91988fac254
Michael Bestas <mikeioannina@gmail.com>
2013-09-11 23:38:48 +0300

    qdsp5v2: Split out audio device for mogami & zeus
    * Bringup audio for mogami & zeus board
    
    Change-Id: Ic4d35509b143fd1dcae8dd049a42ad6123531052

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -657,6 +657,8 @@ static inline void msm_hsusb_set_vbus_state(int online) {}
 
 void msm_snddev_init(void);
 void msm_snddev_init_timpani(void);
+void msm_snddev_hac_amp_on(void);
+void msm_snddev_hac_amp_off(void);
 void msm_snddev_poweramp_on(void);
 void msm_snddev_poweramp_off(void);
 void msm_snddev_hsed_voltage_on(void);
