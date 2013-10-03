00a99e450891ed7cedae360455abaf45e26574c4
Michael Bestas <mikeioannina@gmail.com>
2013-10-03 04:08:00 +0300

    zeus: Configure GPIO 30 for VSYNC.
    Change-Id: I683b5ac0a0e72a4c915989693cddb0abef7e9e55

--- arch/arm/mach-msm/board-semc_zeus-gpiomux.c
+++ arch/arm/mach-msm/board-semc_zeus-gpiomux.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -8,13 +8,43 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  */
-#include <linux/module.h>
-#include <mach/irqs.h>
-#include <mach/gpiomux.h>
 
-static int __init gpiomux_init(void)
+#include <asm/mach-types.h>
+#include <mach/gpio.h>
+#include <mach/gpiomux.h>
+#include <mach/socinfo.h>
+#include "devices.h"
+
+static struct gpiomux_setting mdp_vsync_cfg = {
+	.func = GPIOMUX_FUNC_1,
+	.drv = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_DOWN,
+	.dir = GPIOMUX_IN,
+};
+
+static struct msm_gpiomux_config msm7x30_standard_configs[] __initdata = {
+	{	/* MDP_VSYNC */
+		.gpio = 30,
+		.settings = {
+			[GPIOMUX_SUSPENDED]    = &mdp_vsync_cfg,
+		},
+	},
+};
+
+int __init msm7x30_init_gpiomux(void)
 {
-	return msm_gpiomux_init(NR_GPIO_IRQS);
+	int rc = msm_gpiomux_init(NR_GPIO_IRQS);
+	if (rc) {
+		pr_err(KERN_ERR "msm_gpiomux_init failed %d\n", rc);
+		return rc;
+	}
+
+	msm_gpiomux_install(msm7x30_standard_configs,
+			ARRAY_SIZE(msm7x30_standard_configs));
+
+	return rc;
 }
-postcore_initcall(gpiomux_init);
+
+postcore_initcall(msm7x30_init_gpiomux);
