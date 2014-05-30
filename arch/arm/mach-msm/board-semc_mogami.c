36effbe894be25d3a56dbdd76774757f1afaabbf
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-05-30 19:07:14 +0300

    msm7x30: Enable MM to use CMA heap.
    Using CMA instead of carveout allows the kernel & Android use this space as long
    as there are no clients on it. If a client requests this memory, the area will
    be freed.
    
    Change-Id: I5f31f9ef9739f454e55c2bd8d246cca9c80266a5

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -29,6 +29,7 @@
 #include <linux/input.h>
 #include <linux/power_supply.h>
 #include <linux/msm_adc.h>
+#include <linux/dma-contiguous.h>
 #include <linux/dma-mapping.h>
 #include <linux/regulator/consumer.h>
 
@@ -4137,6 +4138,22 @@ static struct ion_co_heap_pdata co_ion_pdata = {
 	.adjacent_mem_id = INVALID_HEAP_ID,
 	.align = PAGE_SIZE,
 };
+
+static struct ion_co_heap_pdata co_mm_ion_pdata = {
+	.adjacent_mem_id = INVALID_HEAP_ID,
+	.align = PAGE_SIZE,
+};
+
+static u64 msm_dmamask = DMA_BIT_MASK(32);
+
+static struct platform_device ion_mm_heap_device = {
+	.name = "ion-mm-heap-device",
+	.id = -1,
+	.dev = {
+		.dma_mask = &msm_dmamask,
+		.coherent_dma_mask = DMA_BIT_MASK(32),
+	}
+};
 #endif
 
 /**
@@ -4153,10 +4170,11 @@ struct ion_platform_heap msm7x30_heaps[] = {
 		/* MM */
 		{
 			.id	= ION_CP_MM_HEAP_ID,
-			.type	= ION_HEAP_TYPE_CARVEOUT,
+			.type	= ION_HEAP_TYPE_DMA,
 			.name	= ION_MM_HEAP_NAME,
 			.memory_type = ION_EBI_TYPE,
-			.extra_data = (void *)&co_ion_pdata,
+			.extra_data = (void *)&co_mm_ion_pdata,
+			.priv	= (void *)&ion_mm_heap_device.dev,
 		},
 		/* AUDIO */
 		{
@@ -4241,7 +4259,6 @@ static void __init size_ion_devices(void)
 static void __init reserve_ion_memory(void)
 {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_MM_SIZE;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_AUDIO_SIZE;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_SF_SIZE;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_WB_SIZE;
@@ -4276,6 +4293,13 @@ static void __init msm7x30_reserve(void)
 {
 	reserve_info = &msm7x30_reserve_info;
 	msm_reserve();
+#ifdef CONFIG_CMA
+	dma_declare_contiguous(
+			&ion_mm_heap_device.dev,
+			MSM_ION_MM_SIZE,
+			0x0,
+			0x20000000);
+#endif
 #ifdef CONFIG_ANDROID_PERSISTENT_RAM
 	add_persistent_ram();
 #endif
