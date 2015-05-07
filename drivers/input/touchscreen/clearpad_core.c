6992f65f07da6b30bec4bdc4f1ab4a120e2a217d
Michael Bestas <mikeioannina@gmail.com>
2015-05-07 22:55:09 +0300

    input: touchscreen: clearpad: Disable some info messages
    These messages appear when the device wakes from deep sleep and spam the logs.
    They are not critical, so disable them
    
    Change-Id: I5be994aa8ce47913a43929e284fb9e2adc5aefe4

--- drivers/input/touchscreen/clearpad_core.c
+++ drivers/input/touchscreen/clearpad_core.c
@@ -943,7 +943,7 @@ static int synaptics_clearpad_set_power(struct synaptics_clearpad *this)
 
 	if (should_wake && !(active & SYN_ACTIVE_POWER)) {
 
-		dev_info(&this->pdev->dev, "power ON\n");
+		dev_dbg(&this->pdev->dev, "power ON\n");
 
 		synaptics_clearpad_set_irq(this, IRQ_SET_POWER, true);
 		synaptics_read(this, SYNF(F01_RMI, DATA, 0x01), &irq, 1);
@@ -961,7 +961,7 @@ static int synaptics_clearpad_set_power(struct synaptics_clearpad *this)
 
 	} else if (!should_wake && (active & SYN_ACTIVE_POWER)) {
 
-		dev_info(&this->pdev->dev, "power OFF\n");
+		dev_dbg(&this->pdev->dev, "power OFF\n");
 
 		rc = synaptics_put(this, SYNF(F01_RMI, CTRL, 0x00),
 			DEVICE_CONTROL_SLEEP_MODE_SENSOR_SLEEP);
@@ -1849,7 +1849,7 @@ static void synaptics_clearpad_early_suspend(struct early_suspend *handler)
 	struct synaptics_clearpad *this =
 	container_of(handler, struct synaptics_clearpad, early_suspend);
 
-	dev_info(&this->pdev->dev, "early suspend\n");
+	dev_dbg(&this->pdev->dev, "early suspend\n");
 	synaptics_clearpad_pm_suspend(&this->pdev->dev);
 }
 
@@ -1858,7 +1858,7 @@ static void synaptics_clearpad_late_resume(struct early_suspend *handler)
 	struct synaptics_clearpad *this =
 	container_of(handler, struct synaptics_clearpad, early_suspend);
 
-	dev_info(&this->pdev->dev, "late resume\n");
+	dev_dbg(&this->pdev->dev, "late resume\n");
 	synaptics_clearpad_pm_resume(&this->pdev->dev);
 }
 #endif
