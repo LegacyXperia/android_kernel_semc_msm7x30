e30b9f81290f3b435ce8133ad36e2a4b56e02074
Michael Bestas <mikeioannina@gmail.com>
2015-05-07 18:34:02 +0300

    leds: as3676_semc: Disable some info messages
    These messages appear when the device wakes from deep sleep and spam the logs.
    They are not critical, so disable them
    
    Change-Id: I5de85565525ac941e078369744dda254d09167f8

--- drivers/leds/leds-as3676_semc.c
+++ drivers/leds/leds-as3676_semc.c
@@ -1494,7 +1494,7 @@ static int as3676_pm_suspend(struct device *dev)
 	struct i2c_client *client = to_i2c_client(dev);
 	struct as3676_record *rd = i2c_get_clientdata(client);
 
-	dev_info(dev, "Suspending AS3676\n");
+	dev_dbg(dev, "Suspending AS3676\n");
 
 	as3676_lock(rd);
 	rd->cmode = AS3676_CMODE_IMMEDIATE;
@@ -1517,7 +1517,7 @@ static int as3676_pm_resume(struct device *dev)
 	struct i2c_client *client = to_i2c_client(dev);
 	struct as3676_record *rd = i2c_get_clientdata(client);
 
-	dev_info(dev, "Resuming AS3676\n");
+	dev_dbg(dev, "Resuming AS3676\n");
 
 	as3676_lock(rd);
 	as3676_restore(rd);
@@ -1550,7 +1550,7 @@ static void as3676_early_suspend(struct early_suspend *handler)
 	struct as3676_record *rd =
 		container_of(handler, struct as3676_record, early_suspend);
 
-	dev_info(&rd->client->dev, "%s\n", __func__);
+	dev_dbg(&rd->client->dev, "%s\n", __func__);
 
 	as3676_lock(rd);
 	rd->cmode = AS3676_CMODE_IMMEDIATE;
@@ -1571,7 +1571,7 @@ static void as3676_late_resume(struct early_suspend *handler)
 	struct as3676_record *rd =
 		container_of(handler, struct as3676_record, early_suspend);
 
-	dev_info(&rd->client->dev, "%s\n", __func__);
+	dev_dbg(&rd->client->dev, "%s\n", __func__);
 
 	as3676_lock(rd);
 	as3676_restore(rd);
